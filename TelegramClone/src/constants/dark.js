9a97f0f4dc3b387c1501c975d803ac49e1f9251c
Utku İmdat <ut.imd@yandex.com>
2022-09-03 00:39:50 +0300

    Dark.js updated

--- TelegramClone/src/constants/dark.js
+++ TelegramClone/src/constants/dark.js
@@ -2,7 +2,7 @@ export default {
   type: 'dark',
   backgroundColor: '#14202b',
   color: 'white',
-  headerColor: '#2a2a2a',
+  headerColor: '#131c2a',
   textboxColor: 'black',
   imageBackground: 'https://wallpaperaccess.com/full/2224386.jpg',
 };
