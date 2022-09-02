24a5771baf5915a61648981770e39da323b3549e
Utku İmdat <ut.imd@yandex.com>
2022-09-03 00:40:53 +0300

    Go to Chat screen from UserChat screen

--- TelegramClone/src/components/ChatPreview.js
+++ TelegramClone/src/components/ChatPreview.js
@@ -1,20 +1,35 @@
-import {View, Text, Image, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useContext} from 'react';
 import user_list from '../contacts-list.json';
+import {ThemeContext} from '../contexts/theme';
+import {useNavigation} from '@react-navigation/native';
 
 const ChatPreview = ({receiverID}) => {
+  const {theme} = useContext(ThemeContext);
+  const navigation = useNavigation();
   const userTakeMessage = user_list.find(user => user.id === receiverID);
+
+  const gotoUserChat = () => {
+    const userObject = {
+      profilePicture: userTakeMessage.profilePictureURL,
+      firstName: userTakeMessage.firstName,
+      lastName: userTakeMessage.lastName,
+      lastSeen: userTakeMessage.lastSeen,
+      id: userTakeMessage.id,
+    };
+    navigation.navigate('UserChatScreen', {userObject});
+  };
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={gotoUserChat}>
       <Image
         style={styles.profilePicture}
         source={{uri: userTakeMessage.profilePictureURL}}
       />
       <View style={styles.userName}>
-        <Text style={styles.firstName}>{userTakeMessage.firstName}</Text>
-        <Text style={styles.lastName}>{userTakeMessage.lastName}</Text>
+        <Text style={[styles.firstName, {color: theme.color}]}>{userTakeMessage.firstName}</Text>
+        <Text style={[styles.lastName, {color: theme.color}]}>{userTakeMessage.lastName}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
